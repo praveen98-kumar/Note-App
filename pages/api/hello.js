b9429318cc06610b969999c49e83f326fc7aa91f
Praveen Kumar <pkkoram98@gmail.com>
2021-01-05 13:20:19 +0530

    connect to mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectDB from "../../utils/connectDB"
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+connectDB();
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
